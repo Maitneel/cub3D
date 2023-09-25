de391de099aa140a1b034db8bdf5bcf95c92692e
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-09-25 18:08:44 +0900

    follow norm

--- src/get_graphic_info.c
+++ src/get_graphic_info.c
@@ -1,14 +1,12 @@
-#include <sys/errno.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 #include "cub3d_structs.h"
-#include "libft.h"
 #include "get_next_line.h"
-#include "util_lib.h"
+#include "libft.h"
 #include "print_lib.h"
-
+#include "util_lib.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/errno.h>
 
 static bool	is_graphic_info_element_filled(const t_graphic_info *graphic_info)
 {
@@ -19,66 +17,66 @@ static bool	is_graphic_info_element_filled(const t_graphic_info *graphic_info)
 		graphic_info->east_texture != NULL);
 }
 
-static bool is_texture_line(char *line)
+static bool	is_texture_line(char *line)
 {
-	const char *texture_identifier[4] = {"NO ", "SO ", "WE ", "EA "};
-	const size_t texture_identifier_size = 4;
-	size_t i;
+	const char		*texture_identifier[4] = {"NO ", "SO ", "WE ", "EA "};
+	const size_t	texture_identifier_size = 4;
+	size_t			i;
 
 	i = 0;
 	while (i < texture_identifier_size)
 	{
 		if (ft_strncmp(line, texture_identifier[i], 3) == 0)
-			return true;
+			return (true);
 		i++;
 	}
-	return false;
+	return (false);
 }
 
-static bool is_color_line(char *line)
+static bool	is_color_line(char *line)
 {
-	const char *texture_identifier[2] = {"F ", "c "};
-	const size_t texture_identifier_size = 2;
-	size_t i;
+	const char		*texture_identifier[2] = {"F ", "c "};
+	const size_t	texture_identifier_size = 2;
+	size_t			i;
 
 	i = 0;
 	while (i < texture_identifier_size)
 	{
 		if (ft_strncmp(line, texture_identifier[i], 2) == 0)
-			return true;
+			return (true);
 		i++;
 	}
-	return false;
+	return (false);
 }
 
- bool is_correct_texture_format(char *line)
+bool	is_correct_texture_format(char *line)
 {
-	size_t i;
+	size_t	i;
 
 	if (!ft_isspace(line[3]))
-		return false;
+		return (false);
 	i = 4;
 	while (ft_isspace(line[i]) && line[i] != '\0')
 		i++;
 	if (line[i] == '\0')
-		return false;
+		return (false);
 	while (ft_isascii(line[i]) && !ft_isspace(line[i]) && line[i] != '\0')
 		i++;
 	if (line[i] == '\0')
-		return false;
+		return (false);
 	while (ft_isspace(line[i]) && line[i] != '\0')
 		i++;
 	if (line[i] != '\0')
-		return false;
-	return true;
+		return (false);
+	return (true);
 }
 
-bool is_within_0_to_255(char *front)
+bool	is_within_0_to_255(char *front)
 {
-	size_t i;
-	size_t front_index;
-	int converted;
-	
+	size_t	i;
+	size_t	front_index;
+	int		converted;
+
 	converted = 0;
 	i = 0;
 	while (ft_isdigit(front[i]) && converted < 256)
@@ -88,17 +86,15 @@ bool is_within_0_to_255(char *front)
 		i++;
 	}
 	if (converted < 256)
-		return true;
-	return false;
+		return (true);
+	return (false);
 }
 
-bool is_correct_color_format(char *line)
+bool	is_correct_color_format(const char *line)
 {
-	size_t i;
-	size_t j;
+	size_t	i;
+	size_t	j;
 
-	if (!ft_isspace(line[1]))
-		return false;
 	j = 0;
 	i = 2;
 	while (j < 3)
@@ -106,56 +102,56 @@ bool is_correct_color_format(char *line)
 		while (ft_isspace(line[i]) && line[i] != '\0')
 			i++;
 		if (!ft_isdigit(line[i]) || !is_within_0_to_255(&line[i]))
-			return false;
+			return (false);
 		while (ft_isdigit(line[i]))
 			i++;
 		if (line[i] == '\0')
-			return false;
+			return (false);
 		while (ft_isspace(line[i]) && line[i] != '\0')
 			i++;
 		if (line[i] != ',' && j != 2)
-			return false;
+			return (false);
 		i++;
 		j++;
 	}
 	if (line[i - 1] != '\0')
-		return false;
-	return true;
+		return (false);
+	return (true);
 }
 
-static bool is_correct_format(char *line)
+static bool	is_correct_format(char *line)
 {
-	size_t i;
+	size_t	i;
 
 	if (line == NULL)
-		return false;
+		return (false);
 	if (line[0] == '\0')
-		return true;
+		return (true);
 	i = 0;
 	while (line[i] == ' ' && line[i] != '\0')
 		i++;
 	if (is_texture_line(&line[i]))
-		return is_correct_texture_format(&line[i]);
+		return (is_correct_texture_format(&line[i]));
 	if (is_color_line(&line[i]))
-		return is_correct_color_format(&line[i]);
-	else 
-		return false;
+		return (is_correct_color_format(&line[i]));
+	else
+		return (false);
 }
 
-static void set_to_appropriate_element(t_graphic_info *graphic_info, char *line)
+static void	set_to_appropriate_element(t_graphic_info *graphic_info, char *line)
 {
 	// TODO
 }
 
-void free_graphic_info(t_graphic_info *graphic_info)
+void	free_graphic_info(t_graphic_info *graphic_info)
 {
 	// TODO
 }
 
 t_graphic_info	*get_graphic_info(const int fd)
 {
-	t_graphic_info *graphic_info;
-	char *line;
+	t_graphic_info	*graphic_info;
+	char			*line;
 
 	graphic_info = ft_xcalloc(1, sizeof(t_graphic_info));
 	while (is_graphic_info_element_filled(graphic_info))
@@ -163,7 +159,7 @@ t_graphic_info	*get_graphic_info(const int fd)
 		line = get_next_line(fd);
 		if (line == NULL || !is_correct_format(line))
 		{
-			if (errno != 0) 
+			if (errno != 0)
 				print_error(true, "read");
 			else
 				print_error(false, "inccorect map\n");
