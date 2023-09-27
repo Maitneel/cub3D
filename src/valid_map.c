ac204c41692d17f9c0e341026b247ddad6ae3d33
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-09-27 15:50:50 +0900

    implement: is_not_using_incorrect_element func

--- src/valid_map.c
+++ src/valid_map.c
@@ -1,10 +1,26 @@
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 #include "cub3d_structs.h"
 
 static bool	is_not_using_incorrect_element(const t_map_element **map)
 {
-	// TODO
+	size_t i;
+	size_t j;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j] != END_OF_LINE)
+		{
+			if (map[i][j] == INCORRECTED_MAP_CHAR)
+				return (false);
+			j++;
+		}
+		i++;
+	}
 	return (true);
 }
 
