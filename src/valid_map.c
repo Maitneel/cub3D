e7f47b7a0adb10bc0818be73d14caa0f4601971a
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-09-27 12:30:19 +0900

    follow norm

--- src/valid_map.c
+++ src/valid_map.c
@@ -2,18 +2,19 @@
 
 #include "cub3d_structs.h"
 
+static bool	is_not_using_incorrect_element(const t_map_element **map)
+{
+	// TODO
+	return (true);
+}
 
-static bool is_not_using_incorrect_element(const t_map_element **map)
+static bool	is_surrounded_by_wall(const t_map_element **map)
 {
 	// TODO
 	return (true);
 }
-static bool is_surrounded_by_wall(const t_map_element **map)
-{
-	// TODO
-	return (true);
-}
-static bool is_connected_all_element(const t_map_element **map)
+
+static bool	is_connected_all_element(const t_map_element **map)
 {
 	// TODO
 	return (true);
