c21aa001a094758319b049baf8ac7b62055a83ea
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-09-28 18:09:58 +0900

    update: デバッグの出力を別関数化

--- debug/debug.h
+++ debug/debug.h
@@ -11,4 +11,6 @@ void print_cub3d(t_cub3d *cub3d);
 void print_player(t_player player);
 void print_map(t_map_element **map);
 
+void print_key_code(int key_code);
+
 #endif
--- src/hooks.c
+++ src/hooks.c
@@ -12,34 +12,16 @@ int	key_hook(int key_code, void *arg)
 {
 	const t_mlx_hook_arg	*mlx_hook_arg = arg;
 
-	fprintf(stderr, "key_code : '%d'\n", key_code);
 	if (key_code == ARROW_LEFT || key_code == ARROW_RIGHT)
 		rotate_player(key_code, &(mlx_hook_arg->cub3d->player));
 	if (key_code == KEY_A || key_code == KEY_S || key_code == KEY_D || key_code == KEY_W)
 		move_player(key_code, &(mlx_hook_arg->cub3d->player));
-	if (key_code == ARROW_UP)
-		printf("UP\n");
-	if (key_code == ARROW_DOWN)
-		printf("DOWN\n");
-	if (key_code == ARROW_LEFT)
-		printf("LEFT\n");
-	if (key_code == ARROW_RIGHT)
-		printf("RIGHT\n");
-
-	if (key_code == KEY_A)
-		printf("KEY_A\n");
-	if (key_code == KEY_S)
-		printf("KEY_S\n");
-	if (key_code == KEY_D)
-		printf("KEY_D\n");
-	if (key_code == KEY_W)
-		printf("KEY_W\n");
-
 	if (key_code == KEY_ESC)
 	{
 		// TODO
 		exit(0);
 	}
+	print_key_code(key_code);
 	print_player(mlx_hook_arg->cub3d->player);
 	return (0);
 }
