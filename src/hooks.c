2a3fd99c500ea9fb646305b481e19fd47b4f96b3
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-09-28 16:23:01 +0900

    add key_hook debug

--- src/hooks.c
+++ src/hooks.c
@@ -1,11 +1,35 @@
+#include <stdlib.h>
+
 #include "mlx.h"
 #include "cub3d_structs.h"
+#include "mlx_defines.h"
 
 #include <stdio.h>
 
 int	key_hook(int key_code)
 {
-	fprintf(stderr, "key_code : '%d'\n", key_code);
+	if (key_code == ARROW_UP)
+		printf("UP\n");
+	if (key_code == ARROW_DOWN)
+		printf("DOWN\n");
+	if (key_code == ARROW_LEFT)
+		printf("LEFT\n");
+	if (key_code == ARROW_RIGHT)
+		printf("RIGHT\n");
+
+	if (key_code == KEY_A)
+		printf("KEY_A\n");
+	if (key_code == KEY_S)
+		printf("KEY_S\n");
+	if (key_code == KEY_D)
+		printf("KEY_D\n");
+	if (key_code == KEY_W)
+		printf("KEY_W\n");
+
+	if (key_code == KEY_ESC)
+	{
+		exit(0);
+	}
 	return (0);
 }
 
