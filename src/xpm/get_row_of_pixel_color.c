2ba2ac251bf24b5ff228a33d06ff026c764a8d81
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-10-24 04:15:27 +0900

    fix interface

--- src/xpm/get_row_of_pixel_color.c
+++ src/xpm/get_row_of_pixel_color.c
@@ -4,13 +4,6 @@
 
 t_color	*get_row_of_pixel_color(const char *line,
 								const t_color_index_pair *color_map,
-								const size_t height, const size_t width)
+								const size_t width, const size_t char_per_pixel)
 {
-	size_t i;
-
-	i = 0;
-	while (i < height) {
-		fprintf(stderr, "'%s' 0x%02X%02X%02X\n", color_map[i].key, color_map[i].color.red, color_map[i].color.green, color_map[i].color.blue);
-		i++;
-	}
 }
