2f6d154f4136a4623bdc0b1fc7fa2dab6e58a9b8
Maitneel <39688304+Maitneel@users.noreply.github.com>
2023-10-24 00:36:15 +0900

    feat: get_xpm_number_of_color get_xpm_char_per_pixel function

--- include/get_xpm_data.h
+++ include/get_xpm_data.h
@@ -8,6 +8,8 @@
 char	**get_xpm_file_content(const char *filename);
 size_t	get_xpm_height(const char **file_content);
 size_t	get_xpm_width(const char **file_content);
+size_t	get_xpm_number_of_color(const char **file_content);
+size_t	get_xpm_char_per_pixel(const char **file_content);
 t_color	**get_xpm_pixel_color(const char **file_content, const size_t height,
 			const size_t width);
 
