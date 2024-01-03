4f2c0d9355c9472bd6a15f32f41be321f99b774d
taksaito <taksaito@student.42tokyo.jp>
2024-01-03 16:56:52 +0900

    wip
    Co-authored-by: Maitneel <Maitneel@users.noreply.github.com>

--- src/raycasting/new_raycasting_image.c
+++ src/raycasting/new_raycasting_image.c
@@ -14,7 +14,6 @@
 bool is_wall(t_cub3d *cub3d, int y, int x)
 {
     const t_map_element **map = cub3d->map;
-    // if (y < 0 || cub3d->map_height < y || x < 0 || cub3d->map_width < x) {
     if (!(0 < y && y < cub3d->map_height * PLAYER_MAGFICATION) || !(0 <= x && x < cub3d->map_width * PLAYER_MAGFICATION))
     {
         return true;
@@ -37,8 +36,6 @@ t_point *get_collision_point(t_cub3d *cub3d, double dir)
     return new_point(y, x);
 }
 
-
-
 double get_distance(t_player *player, double ray_dir, t_point* start, t_point *end)
 {
     double dtheta = ray_dir - player->direction - M_PI_2; 
@@ -50,6 +47,27 @@ double get_wall_ratio(double wall_distance)
     return (WALL_HEIGHT / (tan(VERT_FOV_ANGLE) * wall_distance));
 }
 
+t_graphic_info *get_graphic_info_by_point(t_cub3d *cub3d, t_point *point)
+{
+    return cub3d->graphic_info->east_texture;
+}
+
+double get_texture_position(t_point *point)
+{      
+    
+    if (point->x % PLAYER_MAGFICATION == 0)
+        return (double)(point->y % PLAYER_MAGFICATION) / (double)PLAYER_MAGFICATION;
+    else if (point->y % PLAYER_MAGFICATION == 0)
+        return (double)(point->x % PLAYER_MAGFICATION) / (double)PLAYER_MAGFICATION;
+    // TODO: 未到達なはず...
+    return 0.0;
+}
+
+void print_point(t_point *point)
+{
+    printf("y: %d, x: %d\n", point->y, point->x);
+}
+
 t_mlx_image	*new_raycasting_image(
 	const t_cub3d *cub3d, const t_mlx *mlx, const int width, const int height)
 {
@@ -58,15 +76,16 @@ t_mlx_image	*new_raycasting_image(
     double          wall_raito;
 
     image = new_image_struct(mlx, width, height);
-    // fprintf(stdout, "direction : %f\n", cub3d->player.direction * 180.0 / M_PI);
     for (int x=0; x<WINDOW_WIDTH; x++)
     {
         double ray_dir = ((cub3d->player.direction - (HN_FOV_ANGLE / 2.0)) + ((HN_FOV_ANGLE / (double)(WINDOW_WIDTH)) * x) + M_PI_2);
-        // fprintf(stdout, "ray_dir : %d\n", ray_dir * 180.0 / M_PI);
         collision_point = get_collision_point(cub3d, ray_dir);
         double wall_dis = get_distance(&cub3d->player, ray_dir, collision_point, &(cub3d->player.point));
         wall_raito = get_wall_ratio(wall_dis);
-        paste_texture(image, wall_raito, 0, cub3d->graphic_info->east_texture, x);
+        paste_texture(image, wall_raito, get_texture_position(collision_point), get_graphic_info_by_point(cub3d, collision_point), x);
+        print_point(collision_point);
+        fprintf(stderr, "pos: %f\n", get_texture_position(collision_point));
+        free(collision_point);
     }
     return (image);
 }
