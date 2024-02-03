5edf2ab8d5ea9693ee6be16377c9ec36286dc5fc
Maitneel <39688304+Maitneel@users.noreply.github.com>
2024-02-03 10:02:55 +0900

    fix: norm

--- src/xpm/get_xpm_size.c
+++ src/xpm/get_xpm_size.c
@@ -107,20 +107,3 @@ size_t	get_xpm_char_per_pixel(const char **file_content)
 	}
 	return (ft_atoi(front));
 }
-
-/*
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char **argv) {
-	char **str = malloc(sizeof(char *) * 2);
-	str[0] = argv[1];
-
-	// str[0] = NULL;
-	fprintf(stderr, "get_xpm_width(argv[1]) : '%zu'\n", get_xpm_width(str));
-	fprintf(stderr, "get_xpm_height(argv[1]) : '%zu'\n", get_xpm_height(str));
-	fprintf(stderr, "get_xpm_number_of_color(argv[1]) : '%zu'\n", get_xpm_number_of_color(str));
-	fprintf(stderr, "get_xpm_char_per_pixel(argv[1]) : '%zu'\n", get_xpm_char_per_pixel(str));
-	
-
-}
-// */
