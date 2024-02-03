e0694b60dc87406d993c186c53b94b0f40aca8ed
Maitneel <39688304+Maitneel@users.noreply.github.com>
2024-02-03 09:30:50 +0900

    fix: norm

--- src/minimap.c
+++ src/minimap.c
@@ -1,11 +1,9 @@
-#include "mlx.h"
-#include <math.h>
-
 #include "cub3d_structs.h"
+#include "free_lib.h"
+#include "mlx.h"
 #include "mlx_image_proc.h"
 #include "util_lib.h"
-#include "free_lib.h"
-
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -16,8 +14,7 @@
 #define MINIMAP_FILED_OF_VIEW_COLOR 0x80ff0000
 
 t_map_element	get_map_element_type(
-	const t_cub3d *cub3d, const int x, const int y
-)
+	const t_cub3d *cub3d, const int x, const int y)
 {
 	t_map_element	type;
 
@@ -60,8 +57,7 @@ void	put_player_position(t_mlx_image *minimap)
 }
 
 unsigned int	**get_default_color_map(
-	const t_cub3d *cub3d, const int width, const int height
-)
+	const t_cub3d *cub3d, const int width, const int height)
 {
 	int	**color_map;
 	int	x;
@@ -77,13 +73,12 @@ unsigned int	**get_default_color_map(
 		j = 0;
 		while (j < width)
 		{
-			// TODO 何が TODO なのかぶっちゃけわからない //
-			x = ((int)(cub3d->player.point.x * 15 / PLAYER_MAGFICATION \
-				+ j) - (width / 2)) / 15;
-			y = ((int)(cub3d->player.point.y * 15 / PLAYER_MAGFICATION \
-				+ i - (height / 2))) / 15;
-			color_map[i][j] = get_minimap_color(get_map_element_type(\
-				cub3d, x, y));
+			x = ((int)(cub3d->player.point.x * 15 / PLAYER_MAGFICATION + j)
+					- (width / 2)) / 15;
+			y = ((int)(cub3d->player.point.y * 15 / PLAYER_MAGFICATION + i
+						- (height / 2))) / 15;
+			color_map[i][j] = get_minimap_color(get_map_element_type(
+						cub3d, x, y));
 			j++;
 		}
 		i++;
@@ -93,8 +88,9 @@ unsigned int	**get_default_color_map(
 
 void	put_line(
 	unsigned int **color_map,
-	const int height, const int width, const double direction
-)
+	const int height,
+	const int width,
+	const double direction)
 {
 	const int	center_x = width / 2;
 	const int	center_y = height / 2;
@@ -107,7 +103,7 @@ void	put_line(
 	magfication = 0;
 	while (0 <= i && i < height && 0 <= j && j < width)
 	{
-		if (color_map[i][j] == MINIMAP_WALL_COLOR || \
+		if (color_map[i][j] == MINIMAP_WALL_COLOR ||
 			color_map[i][j] == MINIMAP_OUT_OF_MAP_COLOR)
 			return ;
 		color_map[i][j] = MINIMAP_FILED_OF_VIEW_COLOR;
@@ -118,8 +114,9 @@ void	put_line(
 }
 
 void	coloring_filed_of_view(const t_cub3d *cub3d,
-	unsigned int **color_map, const int height, const int width
-)
+							unsigned int **color_map,
+							const int height,
+							const int width)
 {
 	size_t	i;
 	int		greater_size;
@@ -133,20 +130,20 @@ void	coloring_filed_of_view(const t_cub3d *cub3d,
 	while (i < greater_size)
 	{
 		// ここ計算量やばい //
-		put_line(color_map, height, width,(cub3d->player.direction + HN_FOV_ANGLE / 2.0 - \
-			((double)(i) * HN_FOV_ANGLE / (double)(greater_size))) + M_PI_2);
+		put_line(color_map, height, width, (cub3d->player.direction
+				+ HN_FOV_ANGLE / 2.0 - ((double)(i)*HN_FOV_ANGLE
+					/ (double)(greater_size))) + M_PI_2);
 		i++;
 	}
 }
 
 void	put_to_image_from_int_array(
-	t_mlx_image *image, unsigned int **color_map
-)
+	t_mlx_image *image, unsigned int **color_map)
 {
 	int	i;
 	int	j;
 
-	i = 0; 
+	i = 0;
 	while (i < image->height)
 	{
 		j = 0;
@@ -160,8 +157,7 @@ void	put_to_image_from_int_array(
 }
 
 t_mlx_image	*new_minimap(
-	const t_cub3d *cub3d, const t_mlx *mlx, const int width, const int height
-)
+	const t_cub3d *cub3d, const t_mlx *mlx, const int width, const int height)
 {
 	t_mlx_image		*minimap;
 	unsigned int	**color_map;
